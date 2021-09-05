d5ada7eb11ad78ae942a05047e2ea5bb50930e3f
KUO SHENG ANG <90085499+kuosheng-ang@users.noreply.github.com>
2021-09-05 22:57:26 +0800

    Update time_series_app.py

--- Time-Series-Forecasting/time_series_app.py
+++ Time-Series-Forecasting/time_series_app.py
@@ -36,7 +36,7 @@ def main():
 	def load_data():
 # 		folder_path = os.path.dirname('GI_data_modified.csv')
 # 		print(folder_path)
-		folder_path = Path(__file__).parents[0]
+		folder_path = Path(__file__).parents[1] + 'datasets/'
 		selected_filename = 'GI_data_modified.csv'
 		GI_df = pd.read_csv(os.path.join(folder_path, selected_filename))
 		return GI_df

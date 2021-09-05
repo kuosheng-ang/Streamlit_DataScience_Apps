a4310b422a268483ffab36ff70358faf115d468c
KUO SHENG ANG <90085499+kuosheng-ang@users.noreply.github.com>
2021-09-05 16:28:49 +0800

    Update app.py

--- common_ml_datasets_explorer_app/app.py
+++ common_ml_datasets_explorer_app/app.py
@@ -34,7 +34,7 @@ def main():
 	
 	
 	def file_selector():
-	    folder_path = "./datasets/"
+	    folder_path = "./datasets"
 	    filenames = os.listdir(folder_path)
 	    selected_filename = st.selectbox('Select a file', filenames)
 	    return os.path.join(folder_path, selected_filename)

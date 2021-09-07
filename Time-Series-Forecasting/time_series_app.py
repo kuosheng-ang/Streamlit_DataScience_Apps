5627027ead3a5aa20cd3e44ad2ac9f9e62d19f47
KUO SHENG ANG <90085499+kuosheng-ang@users.noreply.github.com>
2021-09-07 23:24:10 +0800

    no message

--- Time-Series-Forecasting/time_series_app.py
+++ Time-Series-Forecasting/time_series_app.py
@@ -257,9 +257,9 @@ def arima_model_fcast():
 				ax_ArimaData_plot = ArimaData[i].plot(kind='line', colormap='tab20c')
 				ax_ArimaData_plot.set_xlabel("Shipment Dates",fontsize=15)
 				ax_ArimaData_plot.set_ylabel("Quantity in (Units)",fontsize=15)
-				ax_ArimaData_plot.set_title('Shipment Quantity forecast for'+ i + 'using ARIMA model', fontsize=15)
+				ax_ArimaData_plot.set_title('Shipment Quantity forecast for'+ str(i) + 'using ARIMA model', fontsize=15)
 				st.pyplot(fig)
-				
+
 			except:
 				ArimaFcastPerf[i] = np.nan
 				ArimaData[i] = np.nan

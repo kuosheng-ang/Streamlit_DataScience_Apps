45639e61997461584c7e260712a49bf7cb7f11bc
KUO SHENG ANG <90085499+kuosheng-ang@users.noreply.github.com>
2021-09-07 12:32:10 +0800

    no message

--- Time-Series-Forecasting/time_series_app.py
+++ Time-Series-Forecasting/time_series_app.py
@@ -163,9 +163,9 @@ def descriptive_analysis():
 		GI_Category_Shipment_df = GI_Sales_stats_data.loc[GI_Sales_stats_data['Package'] == selected_product_category]
 		stats_df = GI_Category_Shipment_df.groupby(GI_Category_Shipment_df['GI-Year Month'].dt.strftime('%Y-%m'))[['GI-Year Month','Total Quantity']].sum()
 		st.pyplot(sns.displot(stats_df['Total Quantity']))
-		st.write('Product Package - ' + selected_product_category + ' - Kurtosis of normal distribution: {}'.format(
+		st.write('Product Package - ' + selected_product_category + ' - Kurtosis of normal distribution: {:2f}'.format(
 			stats.kurtosis(stats_df['Total Quantity'])))
-		st.write('Product Package - ' + selected_product_category + ' - Skewness of normal distribution: {}'.format(
+		st.write('Product Package - ' + selected_product_category + ' - Skewness of normal distribution: {:2f}'.format(
 			stats.skew(stats_df['Total Quantity'])))
 
 
